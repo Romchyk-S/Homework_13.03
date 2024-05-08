ae6bbf7af8ff7d95613af2b89c19020b6440b453
Roman Shcherbak <roma.shcherbak1205@gmail.com>
2024-05-08 12:13:01 +0300

    Update find_x_for_piecewise.py
    Changed to include the case of a few disconitunuity points on a period.

--- find_x_for_piecewise.py
+++ find_x_for_piecewise.py
@@ -9,9 +9,8 @@ import math as m
 import numpy as np
 
 def find_x_piecesewise_for_periodic_functions(a: float|int, b: float|int, period: float|int,
-                                              zero_left: float|int, zero_right: float|int, x: np.ndarray):
+                                              zero_left: float|int|list, zero_right: float|int|list, x: np.ndarray):
 
-    
     x_pieces = []
     
     if a < 0:
@@ -22,31 +21,48 @@ def find_x_piecesewise_for_periodic_functions(a: float|int, b: float|int, period
         
         n_range = range(m.ceil((b)/period)+1)
         
-     
-    if a > zero_left:
-        
-        iterations = m.ceil(a/zero_left)
-        
-        zero_left *= iterations
-        
-        zero_right *= iterations
+    if type(zero_left) == list: 
+
+      for zero_left, zero_right in zip(zero_left, zero_right):
+
+       x_pieces = one_cycle_iteration(a, b, period, zero_left, zero_right, x, n_range, x_pieces)
+
+      x_pieces.append(np.logical_not(x_pieces[0]))
     
-    for n_1 in n_range:
-        
+    else:
+
+       x_pieces = one_cycle_iteration(a, b, period, zero_left, zero_right, x, n_range, x_pieces)
+    
+       x_pieces.append(np.logical_not(x_pieces[0]))
+    
+    return x_pieces
+
+def one_cycle_iteration(a: float|int, b: float|int, period: float|int,
+                                              zero_left: float|int|list, zero_right: float|int|list, x: np.ndarray, n_range: range, x_pieces: list):
+    
+     if a > zero_left:
+          
+          iterations = m.ceil(a/zero_left)
+          
+          zero_left *= iterations
+          
+          zero_right *= iterations
+
+     for n_1 in n_range:
+          
         zero_multiply = n_1/abs(n_1) if n_1 != 0 else 0
         
         period_multiply = (n_1-1) if n_1 > 1 else 0
-        
+
         try:
             
             x_pieces[0] = np.logical_or(x_pieces[0], ((x >= zero_multiply*zero_left+period_multiply*period) & 
                                                       (x <= zero_multiply*zero_right+period_multiply*period)))
 
         except IndexError:
-           
-           x_pieces.append((x >= zero_multiply*zero_left+period_multiply*period) & 
-                           (x <= zero_multiply*zero_right+period_multiply*period))
+          
+          x_pieces.append((x >= zero_multiply*zero_left+period_multiply*period) & 
+                          (x <= zero_multiply*zero_right+period_multiply*period))
+
+     return x_pieces
     
-    x_pieces.append(np.logical_not(x_pieces[0]))
-    
-    return x_pieces
